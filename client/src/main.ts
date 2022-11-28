fdd172a716427a1f40fbdc00c5e02cab2bea04e4
Harvy <43960083+ghaowei08@users.noreply.github.com>
2022-11-28 22:18:22 +0800

    i18n

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,5 +1,28 @@
 import { createApp } from 'vue'
+import i18next from 'i18next';
+import I18NextVue from 'i18next-vue';
+import session from './i18n/session.json';
+
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const locales = {
+  en: { ...session.en },
+  ch: { ...session.ch },
+};
+
+i18next.init({
+  lng: 'en',
+  resources: {
+    en: { translation: locales.en },
+    ch: { translation: locales.ch },
+  },
+});
+
+
+const myApp = createApp(App)
+myApp.use(I18NextVue, {
+  i18next: i18next,
+  rerenderOn: ['initialized', 'languageChanged', 'loaded'],
+});
+myApp.mount('#app')
