d35cbb6cee5c0a64926713bb00cbd6a7374489f3
Harvy <43960083+ghaowei08@users.noreply.github.com>
2022-11-28 22:12:18 +0800

    pinia

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,5 +1,10 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import { createPinia } from 'pinia'
 
-createApp(App).mount('#app')
+const pinia = createPinia()
+
+const myApp = createApp(App)
+myApp.use(pinia)
+myApp.mount('#app')
